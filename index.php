c8d3ce888c7be24c607eda3524cffc13daaf8295
Rajyashri Battula <rbattula@cs.brown.edu>
2020-10-06 15:40:58 -0400

    final i think

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <!-- DO NOT ADD/CHANGE ANYTHING IN THIS FILE -->
 <?php include_once("home.html"); ?>
+<!-- PARTS OF CODE TAKEN FROM https://www.w3schools.com/w3css/tryw3css_templates_webpage.htm. -->
